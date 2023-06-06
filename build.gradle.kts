82647e1ea1f4fd81d678a398a30547ac90a0a0b5
Marius Fagerland <mariusfagerland@yahoo.no>
2023-06-07 00:14:16 +0200

    Få opp testcontainers

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,11 @@ repositories {
 }
 
 dependencies {
-	// implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	// runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
--- src/test/kotlin/com/bewcrew/brewcrew/BrewcrewApplicationTests.kt
+++ src/test/kotlin/com/bewcrew/brewcrew/BrewcrewApplicationTests.kt
@@ -8,6 +8,7 @@ class BrewcrewApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		
 	}
 
 }
