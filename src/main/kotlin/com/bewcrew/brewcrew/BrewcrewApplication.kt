2184d76d79838e11ac452c86fb2fd0903cb29e2f
Marius Fagerland <mariusfagerland@yahoo.no>
2023-06-06 23:13:21 +0200

    first commit

---
+++ src/main/kotlin/com/bewcrew/brewcrew/BrewcrewApplication.kt
@@ -0,0 +1,11 @@
+package com.bewcrew.brewcrew
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrewcrewApplication
+
+fun main(args: Array<String>) {
+	runApplication<BrewcrewApplication>(*args)
+}
---
+++ src/test/kotlin/com/bewcrew/brewcrew/BrewcrewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bewcrew.brewcrew
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrewcrewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
